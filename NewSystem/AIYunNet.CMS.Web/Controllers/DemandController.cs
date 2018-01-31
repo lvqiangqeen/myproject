a0417ff29f7ac2c37169831d6677e103b3d6d35b
lvqiangqee <595215839@qq.com>
2018-01-31 10:50:33 +0800

    页面

---
+++ NewSystem/AIYunNet.CMS.Web/Controllers/DemandController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIYunNet.CMS.Web.Controllers
+{
+    public class DemandController : Controller
+    {
+        // GET: Demand
+        public ActionResult DemandList()
+        {
+            return View();
+        }
+    }
+}
