f339cd39bb871b289794fec9859f4e6fd5e0dce9
lvqiangqee <595215839@qq.com>
2018-03-27 18:29:58 +0800

    tijiao

--- NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/MobileWorkerController.cs
+++ NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/MobileWorkerController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AIYunNet.CMS.Web.filter;
 
 namespace AIYunNet.CMS.Web.Areas.MobileApp.Controllers
 {
+    [AllSessionFilter]
     public class MobileWorkerController : Controller
     {
         // GET: MobileApp/MobileWorker
