4f80bb759c2f30660220de7c82d7dac5242cbb22
lvqiangqee <595215839@qq.com>
2018-03-28 17:35:27 +0800

    tijiao

--- NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/MobileWorkerController.cs
+++ NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/MobileWorkerController.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AIYunNet.CMS.Web.filter;
+using AIYunNet.CMS.BLL.Service;
+using AIYunNet.CMS.Domain.Model;
 
 namespace AIYunNet.CMS.Web.Areas.MobileApp.Controllers
 {
     [AllSessionFilter]
     public class MobileWorkerController : Controller
     {
+        WebWorkerService workerSer = new WebWorkerService();
         // GET: MobileApp/MobileWorker
         public ActionResult mWorkerList()
         {
             return View();
         }
-        public ActionResult mWorkerDetail(int workerid)
+        public ActionResult mWorkerDetail(int workerid=1)
         {
-
-            return View();
+            WebWorker worker = workerSer.GetWebWorkerByID(workerid);
+            return View(worker);
         }
     }
 }
