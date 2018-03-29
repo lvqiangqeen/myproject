228c6cf7cf3eaf769e0873483e0554626c12bb67
Administrator <Administrator@JCKJ-20180220NH>
2018-03-30 00:30:47 +0800

    提交

--- NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/MobileCenterController.cs
+++ NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/MobileCenterController.cs
@@ -11,72 +11,29 @@ using AIYunNet.CMS.Common.Utility;
 
 namespace AIYunNet.CMS.Web.Areas.MobileApp.Controllers
 {
+    [MobileUserFilter]
     public class MobileCenterController : Controller
     {
         WebUserService webUserservice = new WebUserService();
         WebPeopleService webpeopleservice = new WebPeopleService();
         WebWorkerService webWorkerService = new WebWorkerService();
         t_AreaService areaService = new t_AreaService();
+        #region 主页
         // GET: MobileApp/MobileCenter
         public ActionResult mPCenter()
         {
-            return View();
+            WebUser user = webUserservice.GetWebUserByID(Convert.ToInt32(SessionHelper.Get("UserID")));
+            return View(user);
         }
-        //登录页面
-        public ActionResult LoginCenter()
+        #endregion
+
+        
+
+        #region 个人资料
+        public ActionResult PersonData()
         {
             return View();
         }
-        [HttpPost]
-        public int LoginOn(string userAccount, string userPassword)
-        {
-            string password = FormsAuthentication.HashPasswordForStoringInConfigFile(userPassword, "md5");
-            bool exist = webUserservice.ExistUser(userAccount, password);
-            if (exist)
-            {
-                WebUser User = webUserservice.GetWebUserByAccount(userAccount, password);
-                WebUserService webuserservice = new WebUserService();
-                WebPeopleService webpeopleservice = new WebPeopleService();
-                //userAccount=UserName
-                if (User.IsLock)
-                {
-
-                    //被锁定
-                    return 100;
-                }
-                else
-                {
-                    SessionHelper.SetSession("UserName", userAccount);
-
-                    WebUser webuser = webuserservice.GetWebUserByAccount(userAccount);
-                    SessionHelper.SetSession("UserID", webuser.UserID);
-                    SessionHelper.SetSession("PositionCode", webuser.PositionCode);
-
-                    WebPeople webpeople = new WebPeople();
-                    WebWorker webWorker = new WebWorker();
-                    if (webpeopleservice.IsHaveuser(webuser.UserID) && webuser.PositionCode == "WebPeople")
-                    {
-                        webpeople = webpeopleservice.GetWebPeopleByUserID(webuser.UserID);
-                        SessionHelper.SetSession("PositionID", webpeople.PeopleID);
-                    }
-                    else if (webWorkerService.IsHaveWorker(webuser.UserID) && (webuser.PositionCode == "WebWorkerLeader" || webuser.PositionCode == "WebWorker"))
-                    {
-                        webWorker = webWorkerService.GetWebWorkerByUserID(webuser.UserID);
-                        SessionHelper.SetSession("PositionID", webWorker.WorkerID);
-                    }
-                    SessionHelper.SetSession("NickName", webuser.NickName);
-                    return 200;
-                }
-            }
-            else
-            {
-                return 500;
-            }
-        }
-        //注册页面
-        public ActionResult registCenter()
-        {
-            return View();
-        }
+        #endregion
     }
 }
---
+++ NewSystem/AIYunNet.CMS.Web/filter/MobileUserFilter.cs
@@ -0,0 +1,24 @@
+﻿using AIYunNet.CMS.Common.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIYunNet.CMS.Web.filter
+{
+    public class MobileUserFilter : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpRequestBase request = filterContext.RequestContext.HttpContext.Request;
+            string userAcount = SessionHelper.Get("userName");
+            if (string.IsNullOrEmpty(userAcount))
+            {
+                filterContext.Result = new RedirectResult(string.Format("/MobileApp/mLogin/LoginCenter"));
+                return;
+            }
+
+        }
+    }
+}
