b7d9f02cca65ceb36c63cded5669807ab13cda50
lvqiangqee <595215839@qq.com>
2018-04-13 17:40:30 +0800

    tijiao1

--- NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/mOrderController.cs
+++ NewSystem/AIYunNet.CMS.Web/Areas/MobileApp/Controllers/mOrderController.cs
@@ -17,6 +17,7 @@ namespace AIYunNet.CMS.Web.Areas.MobileApp.Controllers
         DecDemandAcceptService AcSer = new DecDemandAcceptService();
         DemandService deSer = new DemandService();
         WebBuidingService buidSer = new WebBuidingService();
+        WebBuidingStagesService stageSer = new WebBuidingStagesService();
         #region 订单
         // GET: MobileApp/mOrder
         public ActionResult Orderlist()
@@ -65,7 +66,22 @@ namespace AIYunNet.CMS.Web.Areas.MobileApp.Controllers
 
         public ActionResult mobileBuidingDetail(int BuidingID)
         {
-            return View();
+            WebBuiding buiding = buidSer.GetWebBuidingByID(BuidingID);
+            List<WebBuidingStages> stageslist = stageSer.GetWebBuidingStagesListByBuiding(BuidingID);
+            DecDemand demand = deSer.GetDecDemandByGuID(buiding.Guid);
+            ViewBag.stageslist = stageslist;
+            ViewBag.demand = demand;
+            return View(buiding);
+        }
+
+        public ActionResult mobileStageDetail(int BuidingID, int stageid)
+        {
+            WebBuiding buiding = buidSer.GetWebBuidingByID(BuidingID);
+            List<WebBuidingStages> stageslist = stageSer.GetWebBuidingStagesListByBuiding(BuidingID);
+            DecDemand demand = deSer.GetDecDemandByGuID(buiding.Guid);
+            ViewBag.stageslist = stageslist;
+            ViewBag.demand = demand;
+            return View(buiding);
         }
         #endregion
     }
